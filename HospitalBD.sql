54c9ee48269f72f7d50982e575eae4a28f006788
Naza2006 <nazario@uamv.edu.ni>
2026-06-04 08:42:59 -0600

    Cree la base de datos

---
+++ HospitalBD.sql
@@ -0,0 +1,12 @@
+--1.Crear base de datos
+CREATE DATABASE HospitalDB;
+GO
+
+--2.Mostrar bases de datos existentes
+SELECT name
+FROM sys.databases;
+GO
+
+--3.Seleccionar HospitalDB
+USE HospitalDB;
+GO
